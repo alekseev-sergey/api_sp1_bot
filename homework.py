8d75d682af4fc71d6cebdbdcbe33925450fe3862
alekseev-sergey <alekseev-sergey23@yandex.ru>
2021-05-07 16:17:26 +0300

    My 3th commit

--- homework.py
+++ homework.py
@@ -16,7 +16,7 @@ logging.basicConfig(
     format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
 )
 
-PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
+PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 API_URL = 'https://praktikum.yandex.ru/api/user_api/'
